d996c52c980abf2b712e910a1388868472aaa940
macmillianzungene <macmillianzungene@gmail.com>
2025-02-10 01:51:46 +0200

    added more features and updated the import the css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,8 @@ import Login from "./Login";
 import Dashboard from "./Dashboard";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
