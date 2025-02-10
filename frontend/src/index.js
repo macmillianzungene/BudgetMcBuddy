a0da544a9880eac15735d7f95a89ad5eca90e978
macmillianzungene <macmillianzungene@gmail.com>
2025-02-10 02:04:45 +0200

    only imported the css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import { AuthProvider } from "./authContext"; // Import AuthProviderimport
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
